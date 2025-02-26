32df0e89d2c077f87f613e0dd3f22c099136fa80
Joe Lanford <joe.lanford@gmail.com>
2025-02-25 21:21:55 -0500

    experiment: auto-create permissions from preflight check results
    Signed-off-by: Joe Lanford <joe.lanford@gmail.com>

--- go.mod
+++ go.mod
@@ -8,7 +8,6 @@ require (
 	github.com/containerd/platforms v0.2.1
 	github.com/onsi/ginkgo v1.16.5
 	github.com/onsi/gomega v1.36.2
-	github.com/open-policy-agent/opa v0.68.0
 	github.com/opencontainers/image-spec v1.1.1
 	github.com/operator-framework/api v0.30.0
 	github.com/operator-framework/operator-controller v1.2.0
@@ -30,12 +29,8 @@ require (
 	github.com/BurntSushi/toml v1.4.0 // indirect
 	github.com/Microsoft/go-winio v0.6.2 // indirect
 	github.com/Microsoft/hcsshim v0.12.9 // indirect
-	github.com/OneOfOne/xxhash v1.2.8 // indirect
-	github.com/agnivade/levenshtein v1.1.1 // indirect
 	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
-	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/blang/semver/v4 v4.0.0 // indirect
-	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/containerd/cgroups/v3 v3.0.3 // indirect
 	github.com/containerd/containerd/api v1.8.0 // indirect
 	github.com/containerd/continuity v0.4.4 // indirect
@@ -65,19 +60,17 @@ require (
 	github.com/go-git/gcfg v1.5.1-0.20230307220236-3a3c6141e376 // indirect
 	github.com/go-git/go-billy/v5 v5.6.1 // indirect
 	github.com/go-git/go-git/v5 v5.13.1 // indirect
-	github.com/go-ini/ini v1.67.0 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-openapi/jsonpointer v0.21.0 // indirect
 	github.com/go-openapi/jsonreference v0.21.0 // indirect
 	github.com/go-openapi/swag v0.23.0 // indirect
-	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/cel-go v0.22.1 // indirect
 	github.com/google/gnostic-models v0.6.9 // indirect
-	github.com/google/go-cmp v0.7.0 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
@@ -106,27 +99,17 @@ require (
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/runtime-spec v1.2.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/prometheus/client_golang v1.21.1 // indirect
-	github.com/prometheus/client_model v0.6.1 // indirect
-	github.com/prometheus/common v0.62.0 // indirect
-	github.com/prometheus/procfs v0.15.1 // indirect
-	github.com/rcrowley/go-metrics v0.0.0-20200313005456-10cdbea86bc0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/stoewer/go-strcase v1.3.0 // indirect
-	github.com/tchap/go-patricia/v2 v2.3.1 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
-	github.com/xeipuuv/gojsonpointer v0.0.0-20190905194746-02993c407bfb // indirect
-	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
-	github.com/yashtewari/glob-intersection v0.2.0 // indirect
 	go.etcd.io/bbolt v1.3.11 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.58.0 // indirect
 	go.opentelemetry.io/otel v1.33.0 // indirect
 	go.opentelemetry.io/otel/metric v1.33.0 // indirect
-	go.opentelemetry.io/otel/sdk v1.33.0 // indirect
 	go.opentelemetry.io/otel/trace v1.33.0 // indirect
-	golang.org/x/exp v0.0.0-20250228200357-dead58393ab7 // indirect
+	golang.org/x/exp v0.0.0-20241009180824-f66d83c29e7c // indirect
 	golang.org/x/net v0.35.0 // indirect
 	golang.org/x/oauth2 v0.27.0 // indirect
 	golang.org/x/sync v0.11.0 // indirect
@@ -143,12 +126,12 @@ require (
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 	gopkg.in/warnings.v0 v0.1.2 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	k8s.io/klog/v2 v2.130.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20241212222426-2c72e554b1e7 // indirect
 	k8s.io/utils v0.0.0-20241210054802-24370beab758 // indirect
-	oras.land/oras-go/v2 v2.3.1 // indirect
 	sigs.k8s.io/json v0.0.0-20241014173422-cfa47c3a1cc8 // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.5.0 // indirect
 )
+
+replace github.com/operator-framework/operator-controller => github.com/joelanford/operator-controller v0.0.0-20250226022008-221976e3e731
--- internal/pkg/action/v1/operator_install.go
+++ internal/pkg/action/v1/operator_install.go
@@ -2,13 +2,16 @@ package v1
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"hash/fnv"
+	"k8s.io/apimachinery/pkg/types"
+	"math/big"
 	"time"
 
-	"errors"
 	"github.com/operator-framework/kubectl-operator/pkg/action"
 	ocv1 "github.com/operator-framework/operator-controller/api/v1"
-	corev1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -22,17 +25,15 @@ import (
 type OperatorInstall struct {
 	config *action.Configuration
 
-	UnsafeCreateClusterRoleBinding bool
-
 	Namespace      OperatorInstallNamespaceConfig
 	ServiceAccount string
 
-	Package         string
-	Channels        []string
-	Version         string
-	CatalogSelector metav1.LabelSelector
-
-	CleanupTimeout time.Duration
+	Package             string
+	Channels            []string
+	Version             string
+	CatalogSelector     metav1.LabelSelector
+	MissingRulesHandler func(context.Context, *ocv1.ClusterExtension) (bool, error)
+	CleanupTimeout      time.Duration
 
 	Logf func(string, ...interface{})
 }
@@ -66,11 +67,23 @@ func (i *OperatorInstall) applyServiceAccount(ctx context.Context) error {
 	return patchObject(ctx, i.config.Client, ac)
 }
 
-func (i *OperatorInstall) applyClusterRoleBinding(ctx context.Context, clusterRoleName string) error {
-	name := fmt.Sprintf("kubectl-operator-%s-cluster-admin", i.ServiceAccount)
-	ac := applyconfigurationsrbacv1.ClusterRoleBinding(name).
-		WithSubjects(applyconfigurationsrbacv1.Subject().WithNamespace(i.Namespace.Name).WithKind("ServiceAccount").WithName(i.ServiceAccount)).
-		WithRoleRef(applyconfigurationsrbacv1.RoleRef().WithKind("ClusterRole").WithName(clusterRoleName))
+func (i *OperatorInstall) applyClusterRole(ctx context.Context, key types.NamespacedName, labels map[string]string, rules []*applyconfigurationsrbacv1.PolicyRuleApplyConfiguration) error {
+	ac := applyconfigurationsrbacv1.ClusterRole(key.Name).WithLabels(labels).WithRules(rules...)
+	return patchObject(ctx, i.config.Client, ac)
+}
+
+func (i *OperatorInstall) applyRole(ctx context.Context, key types.NamespacedName, labels map[string]string, rules []*applyconfigurationsrbacv1.PolicyRuleApplyConfiguration) error {
+	ac := applyconfigurationsrbacv1.Role(key.Name, key.Namespace).WithLabels(labels).WithRules(rules...)
+	return patchObject(ctx, i.config.Client, ac)
+}
+
+func (i *OperatorInstall) applyClusterRoleBinding(ctx context.Context, key types.NamespacedName, labels map[string]string, subject *applyconfigurationsrbacv1.SubjectApplyConfiguration, roleRef *applyconfigurationsrbacv1.RoleRefApplyConfiguration) error {
+	ac := applyconfigurationsrbacv1.ClusterRoleBinding(key.Name).WithLabels(labels).WithSubjects(subject).WithRoleRef(roleRef)
+	return patchObject(ctx, i.config.Client, ac)
+}
+
+func (i *OperatorInstall) applyRoleBinding(ctx context.Context, key types.NamespacedName, labels map[string]string, subject *applyconfigurationsrbacv1.SubjectApplyConfiguration, roleRef *applyconfigurationsrbacv1.RoleRefApplyConfiguration) error {
+	ac := applyconfigurationsrbacv1.RoleBinding(key.Name, key.Namespace).WithLabels(labels).WithSubjects(subject).WithRoleRef(roleRef)
 	return patchObject(ctx, i.config.Client, ac)
 }
 
@@ -115,21 +128,17 @@ func (i *OperatorInstall) Run(ctx context.Context) (*ocv1.ClusterExtension, erro
 		return nil, fmt.Errorf("apply service account %q: %v", i.ServiceAccount, err)
 	}
 
-	if i.UnsafeCreateClusterRoleBinding {
-		if err := i.applyClusterRoleBinding(ctx, "cluster-admin"); err != nil {
-			return nil, fmt.Errorf("apply cluster role binding: %v", err)
-		}
-	}
-
 	if err := i.applyClusterExtension(ctx); err != nil {
 		return nil, fmt.Errorf("apply cluster extension: %v", err)
 	}
 
 	clusterExtension, err := i.waitForClusterExtensionInstalled(ctx)
 	if err != nil {
+		err = fmt.Errorf("wait for cluster extension installation: %v", err)
 		cleanupCtx, cancelCleanup := context.WithTimeout(context.Background(), i.CleanupTimeout)
 		defer cancelCleanup()
 		cleanupErr := i.cleanup(cleanupCtx)
+		cleanupErr = fmt.Errorf("error cleaning up cluster extension installation: %w", err)
 		return nil, errors.Join(err, cleanupErr)
 	}
 	return clusterExtension, nil
@@ -147,6 +156,23 @@ func (i *OperatorInstall) waitForClusterExtensionInstalled(ctx context.Context) 
 		if err := i.config.Client.Get(conditionCtx, key, clusterExtension); err != nil {
 			return false, err
 		}
+
+		observedGeneration := int64(-1)
+		for _, cond := range clusterExtension.Status.Conditions {
+			observedGeneration = max(observedGeneration, cond.ObservedGeneration)
+		}
+
+		if observedGeneration != clusterExtension.Generation {
+			return false, nil
+		}
+
+		if clusterExtension.Status.Rules != nil && len(clusterExtension.Status.Rules.Missing) > 0 && i.MissingRulesHandler != nil {
+			done, err := i.MissingRulesHandler(ctx, clusterExtension)
+			if err != nil || !done {
+				return false, err
+			}
+		}
+
 		progressingCondition := meta.FindStatusCondition(clusterExtension.Status.Conditions, ocv1.TypeProgressing)
 		if progressingCondition != nil && progressingCondition.Reason != ocv1.ReasonSucceeded {
 			errMsg = progressingCondition.Message
@@ -166,29 +192,102 @@ func (i *OperatorInstall) waitForClusterExtensionInstalled(ctx context.Context) 
 }
 
 func (i *OperatorInstall) cleanup(ctx context.Context) error {
-	clusterExtension := &ocv1.ClusterExtension{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: i.Package,
-		},
+	u := OperatorUninstall{
+		config:  i.config,
+		Package: i.Package,
+		Logf:    i.Logf,
 	}
-	clusterRoleBinding := &rbacv1.ClusterRoleBinding{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: fmt.Sprintf("kubectl-operator-%s-cluster-admin", i.ServiceAccount),
-		},
+	return u.Run(ctx)
+}
+
+func (i *OperatorInstall) AutoCreateMissingRules(ctx context.Context, clusterExtension *ocv1.ClusterExtension) (bool, error) {
+
+	subject := rbacv1.Subject{
+		Kind:      "ServiceAccount",
+		Name:      i.ServiceAccount,
+		Namespace: i.Namespace.Name,
 	}
-	serviceAccount := &corev1.ServiceAccount{
-		ObjectMeta: metav1.ObjectMeta{
-			Namespace: i.Namespace.Name,
-			Name:      i.ServiceAccount,
-		},
+
+	if clusterExtension.Status.Rules == nil || len(clusterExtension.Status.Rules.Missing) == 0 {
+		return true, nil
 	}
-	namespace := &corev1.Namespace{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: i.Namespace.Name,
-		},
+
+	assumeDone := len(clusterExtension.Status.Rules.Missing) < 64
+	for _, missingRuleItem := range clusterExtension.Status.Rules.Missing {
+		assumeDone = assumeDone && len(missingRuleItem.Rules) < 1024
+
+		if len(missingRuleItem.Rules) == 0 {
+			continue
+		}
+
+		nameDigest, err := hashToString(missingRuleItem.Rules)
+		if err != nil {
+			return false, err
+		}
+		roleKey := types.NamespacedName{Namespace: missingRuleItem.Namespace, Name: fmt.Sprintf("olmv1:clusterextension:%s:%s", clusterExtension.Name, nameDigest)}
+		bindingKey := types.NamespacedName{Namespace: missingRuleItem.Namespace, Name: fmt.Sprintf("olmv1:clusterextension:%s:sa:%s:%s", clusterExtension.Name, clusterExtension.Spec.ServiceAccount.Name, nameDigest)}
+
+		applyRole := i.applyClusterRole
+		applyBinding := i.applyClusterRoleBinding
+		roleRef := rbacv1.RoleRef{APIGroup: rbacv1.GroupName, Kind: "ClusterRole", Name: roleKey.Name}
+		roleResource := "clusterrole"
+		bindingResource := "clusterrolebinding"
+		if missingRuleItem.Namespace != "" {
+			applyRole = i.applyRole
+			applyBinding = i.applyRoleBinding
+			roleRef.Kind = "Role"
+			roleResource = "role"
+			bindingResource = "rolebinding"
+		}
+
+		labels := map[string]string{
+			"olm.operatorframework.io/kubectl-operator-autogenerated": "true",
+			"olm.operatorframework.io/cluster-extension-name":         clusterExtension.Name,
+		}
+
+		var acRules []*applyconfigurationsrbacv1.PolicyRuleApplyConfiguration
+		for _, rule := range missingRuleItem.Rules {
+			acRules = append(acRules, applyconfigurationsrbacv1.PolicyRule().
+				WithAPIGroups(rule.APIGroups...).
+				WithResources(rule.Resources...).
+				WithVerbs(rule.Verbs...).
+				WithResourceNames(rule.ResourceNames...).
+				WithNonResourceURLs(rule.NonResourceURLs...),
+			)
+		}
+
+		if err := applyRole(ctx, roleKey, labels, acRules); err != nil {
+			return false, fmt.Errorf("apply %s %q: %w", roleResource, roleKey, err)
+		}
+		if err := applyBinding(ctx, bindingKey, labels,
+			applyconfigurationsrbacv1.Subject().
+				WithAPIGroup(subject.APIGroup).
+				WithKind(subject.Kind).
+				WithNamespace(subject.Namespace).
+				WithName(subject.Name),
+			applyconfigurationsrbacv1.RoleRef().
+				WithAPIGroup(roleRef.APIGroup).
+				WithKind(roleRef.Kind).
+				WithName(roleRef.Name),
+		); err != nil {
+			return false, fmt.Errorf("apply %s %q: %w", bindingResource, bindingKey, err)
+		}
+
 	}
-	if err := deleteAndWait(ctx, i.config.Client, clusterExtension); err != nil {
-		return fmt.Errorf("delete clusterextension %q: %v", i.Package, err)
+	return assumeDone, nil
+}
+
+func hashToString(itemToHash any) (string, error) {
+	digester := fnv.New64a()
+	digestEncoder := json.NewEncoder(digester)
+	if err := digestEncoder.Encode(itemToHash); err != nil {
+		return "", err
 	}
-	return deleteAndWait(ctx, i.config.Client, clusterRoleBinding, serviceAccount, namespace)
+
+	var digest []byte
+	digest = digester.Sum(digest)
+
+	var i big.Int
+	i.SetBytes(digest[:])
+	return i.Text(36), nil
 }
